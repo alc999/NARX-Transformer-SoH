bf776f76148a6e0c1af1fbd3a7f670d320774a44
Amirhossein Heydarian <amirhossein261077@live.com>
2023-10-19 18:40:36 +0330

    made it flexible for multiple cycles

--- dataset.py
+++ dataset.py
@@ -1,20 +1,17 @@
 from torch.utils.data import Dataset, DataLoader
 
 class BatteryDataset(Dataset):
-    def __init__(self, battery_data):
-        self.battery_data = battery_data
+    def __init__(self, battery_dict, num_cycles):
+        self.battery_data = []
+        for battery_type in battery_dict:
+            for i in range(len(battery_dict[battery_type]['data'])-num_cycles+1):
+                sequences = battery_dict[battery_type]['data'][i:i+num_cycles]
+                capacities = battery_dict[battery_type]['cap'][i:i+num_cycles]
+                self.battery_data.append([battery_type,sequences,capacities])
 
     def __len__(self):
         return len(self.battery_data)
 
     def __getitem__(self, index):
-        name, data, cap = self.battery_data[index]
-
-        if index > 0:
-            old_name, _, old_cap = self.battery_data[index-1]
-            if old_name!=name:
-                old_cap=1
-        else:
-            old_cap = 1
-
-        return data, old_cap, cap
+        name, data, caps = self.battery_data[index]
+        return data, caps
