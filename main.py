2035312e8e0c4d8d25339f30ca2710eeb85a1f9f
Amirhossein Heydarian <amirhossein261077@live.com>
2023-10-23 11:01:41 +0330

    train test split

--- main.py
+++ main.py
@@ -29,8 +29,13 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # Load data
 battery_dict = load_NASA(folder='NASA_DATA', scale_data=True)
-dataset = BatteryDataset(battery_dict, num_cycles=NUM_CYCLES)
-dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True)
+battery_dict_no18 = {key: val for key, val in battery_dict.items() if key != 'B0018'}
+dataset = BatteryDataset(battery_dict_no18, num_cycles=NUM_CYCLES)
+
+# Train/test split
+train_dataset, test_dataset = train_test_split(dataset, test_size=0.2, random_state=42)
+train_dataloader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+test_dataloader  = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=True)
 
 # NN model
 model = CNN_Transformer(feature_dim=FEATURE_DIM, num_cycles=NUM_CYCLES).to(device)
@@ -45,35 +50,32 @@ model.train()
 
 t_range = trange(EPOCHS)
 for epoch in t_range:
-    losses = []
-    for inputs, outputs in dataloader:
-        # Convert inputs and outputs to PyTorch tensors
+    train_losses = []
+    for inputs, outputs in train_dataloader:
         inputs = inputs.float().to(device)
         outputs = outputs.float().to(device)
-
-        # Zero the gradients
-        optimizer.zero_grad()
-
-        # Forward pass
         predicted_outputs = model(inputs, outputs[:,:-1])
 
-        # Compute the loss
+        optimizer.zero_grad()
         loss = criterion(predicted_outputs, outputs[:,-1].unsqueeze(-1))
-
-        # Backward pass
         loss.backward()
-
-        # Update the weights
         optimizer.step()
+        train_losses.append(loss.item())
 
-        losses.append(loss.item())
-
+    test_losses = []
+    for inputs, outputs in test_dataloader:
+        inputs = inputs.float().to(device)
+        outputs = outputs.float().to(device)
+        with torch.no_grad():
+            predicted_outputs = model(inputs, outputs[:,:-1])
+            test_loss = criterion(predicted_outputs, outputs[:,-1].unsqueeze(-1))
+            test_losses.append(test_loss.item())
 
     # Print the loss for monitoring after each epoch
-    t_range.set_description(f"loss: {np.mean(losses)}")
+    t_range.set_description(f"train loss: {np.mean(train_losses)}, test loss: {np.mean(test_losses)}")
     t_range.refresh()
-    
+
     # Check if the current loss is the best so far
-    if np.mean(losses) < best_loss:
-        best_loss = np.mean(losses)
+    if np.mean(test_losses) < best_loss:
+        best_loss = np.mean(test_losses)
         torch.save(model, 'trained_model.pt')
