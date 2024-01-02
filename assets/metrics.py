11ed4c94a23d8ebbe537a63f1129b125b36d3b5b
amirhosseinh77 <amirhossein261077@live.com>
2024-01-02 15:16:38 +0330

    evaluation tools

--- assets/metrics.py
+++ assets/metrics.py
@@ -1,2 +1,9 @@
 import numpy as np
-from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.metrics import mean_squared_error, mean_absolute_error, max_error
+
+def report_metrics(x,  x_hat):
+    MSE = mean_squared_error(x, x_hat)
+    RMSE = np.sqrt(MSE)
+    MAE = mean_absolute_error(x, x_hat)
+    MAX_ERR = max_error(x, x_hat)
+    return MSE, RMSE, MAE, MAX_ERR
--- assets/plots.py
+++ assets/plots.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.stats import norm
 import torch
 
 def plot_capacity_sequence(model, battery_dict, num_cycles):
@@ -25,6 +26,7 @@ def plot_predicted_capacity(model, train_dataset, test_dataset, num_cycles):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     battery_types = train_dataset.battery_data[:,0]
 
+    evaluations = []
     for battery_type in np.unique(battery_types):
         train_data = train_dataset.battery_data[train_dataset.battery_data[:,0] == battery_type]
         test_data = test_dataset.battery_data[test_dataset.battery_data[:,0] == battery_type]
@@ -40,11 +42,36 @@ def plot_predicted_capacity(model, train_dataset, test_dataset, num_cycles):
             pred = model(inputs, outputs)
             pred_caps.append(pred.item())
         
-        plt.plot(train_caps+[test_caps[0]], 'blue')
+        evaluations.append([battery_type, np.array(test_caps), np.array(pred_caps[num_cycles-1:])])
+
+        plt.figure(figsize=(8,5))
+        plt.plot(train_caps+[test_caps[0]])
         plt.plot(range(len(train_caps),len(train_caps)+len(test_caps)), test_caps, c='olive')
         plt.plot(range(len(train_caps),len(train_caps)+len(pred_caps)-num_cycles+1), pred_caps[num_cycles-1:], '--', c='red')
         plt.grid('on')
-        plt.legend(['Real','Label','Predicted'])
-        plt.xlabel('Cycle Number')
-        plt.ylabel('SoH (%)')
+        plt.title(battery_type, fontsize=15)
+        plt.legend(['_','Real data','Prediction'], loc='upper right')
+        plt.xlabel('Cycle')
+        plt.ylabel('Battery SoH (%)')
         plt.show()
+
+    return evaluations
+
+
+def plot_error_histogram(errors):
+    plt.figure(figsize=(8,5))
+    plt.xlim([-0.1,0.1])
+    counts, bins, _ = plt.hist(errors, bins=10, density=False, alpha=0.7, label='Error Distribution')
+
+    # Fit a Gaussian distribution to the data
+    mu, std = norm.fit(errors)
+    xmin, xmax = plt.xlim()
+    x = np.linspace(xmin, xmax, 100)
+    p = norm.pdf(x, mu, std) * np.sum(counts) / 200  
+    plt.plot(x, p, 'k', linewidth=2, label=f'Gaussian Fit\n$\mu={mu:.2f}$, $\sigma={std:.2f}$')
+
+    plt.xlabel('Error', fontsize=15)
+    plt.ylabel('Counts', fontsize=15)
+    # plt.title('Distribution of Errors with Gaussian Fit')
+    plt.legend(loc='upper right')
+    plt.show()
